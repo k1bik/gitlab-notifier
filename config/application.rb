384666535d139f5af72cdbdde2920d1232542ae2
Kirill Biller <biller.k@yandex.ru>
2025-09-06 16:36:18 +0300

    Production setup

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module GitlabNotifier
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configure Active Job to use Sidekiq
+    config.active_job.queue_adapter = :sidekiq
   end
 end
