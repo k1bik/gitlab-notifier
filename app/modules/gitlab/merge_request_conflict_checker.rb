3389211ef964865beb372608fc88975e7f8f5384
Kirill Biller <biller.k@yandex.ru>
2025-11-14 22:57:33 +0300

    Remove caching of last check time in merge request conflict checker, simplifying the logic for fetching opened merge requests with conflicts.

--- app/modules/gitlab/merge_request_conflict_checker.rb
+++ app/modules/gitlab/merge_request_conflict_checker.rb
@@ -7,18 +7,11 @@ module Gitlab
       return unless current_hour.between?(9, 19)
 
       gitlab_service = Gitlab::Service.new
-      last_check_key = "mr_conflict_checker:last_check"
-      last_check = Rails.cache.fetch(last_check_key, expires_in: 1.hour) { 15.minutes.ago }
-
-      puts "Last check: #{last_check}"
 
       merge_requests = gitlab_service.get_merge_requests do |request|
         request.params["state"] = "opened"
-        request.params["per_page"] = 50
-        request.params["updated_after"] = last_check.iso8601
+        request.params["per_page"] = 70
       end
-
-      Rails.cache.write(last_check_key, Time.current)
 
       merge_requests.each do |merge_request|
         next if merge_request["detailed_merge_status"] != "conflict"
